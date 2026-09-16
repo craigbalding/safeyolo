4dc6d6a950df07b2e442103314aac511add040a7
craigb <craig@threatspotting.com>
2026-09-16 22:53:18 +0000

    fix(blackbox): remove duplicate readiness state

--- tests/proxy_migration/conftest.py
+++ tests/proxy_migration/conftest.py
@@ -4,8 +4,6 @@ import pytest
 
 _READINESS_FAILURE = False
 
-_READINESS_FAILURE = False
-
 
 def pytest_addoption(parser):
     parser.addoption("--proxy-backend", action="append", choices=("python", "rust"), default=[])
